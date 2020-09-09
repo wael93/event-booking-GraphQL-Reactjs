cf2d305bf0719c1be91c232b27d8c8a39f727604
Wael Achi <waelashi1@gmail.com>
2020-09-09 13:08:10 +0300

    Built a Server

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const
+const express = require('express');
+const bodyparser = require ('body-parser');
+const app = express();
+
+app.use(bodyparser.json());
+app.get('/',(req,res,next)=>{
+    res.send('Wael Event Booking ');
+});
+app.listen(3000);
