777117fa19032735098d010064ebe2296ba54c84
Wael Achi <waelashi1@gmail.com>
2020-09-12 15:27:52 +0300

    build GraphQl Recever

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const bodyparser = require ('body-parser');
-const app = express();
-const graphQlHttp = require('express-graphql');
-const {buildSchema} = require('graphql');
+const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-app.use(bodyparser.json());
+const app = express();
+
+app.use(bodyParser.json());
+
 app.use(
-    '/graphql',
-    graphQlHttp({
-        schema: buildSchema(`
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
         type RootQuery {
             events: [String!]!
         }
@@ -20,24 +22,17 @@ app.use(
             mutation: RootMutation
         }
     `),
-    // Recerver Function 
     rootValue: {
-        events: () => {
-          return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
-        },
-        createEvent: (args) => {
-          const eventName = args.name;
-          return eventName;
-        }
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
       },
-      graphiql: true
-    })
-  );
-  
-  app.listen(3000);
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
-
-app.get('/',(req,res,next)=>{
-    res.send('Wael Event Booking ');
-});
 app.listen(3000);
